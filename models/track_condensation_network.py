c7c8ca7a4d97f04bc952a55740f45446cebc827f
Kilian Lieret <kilian.lieret@posteo.de>
2022-07-22 10:44:45 -0400

    Reformat code with black

--- models/track_condensation_network.py
+++ models/track_condensation_network.py
@@ -8,6 +8,7 @@ import torch_geometric.transforms as T
 from torch_geometric.nn import MessagePassing
 from models.interaction_network import InteractionNetwork as IN
 
+
 class MLP(nn.Module):
     def __init__(self, input_size, output_size, hidden_size):
         super(MLP, self).__init__()
@@ -25,25 +26,17 @@ class MLP(nn.Module):
     def forward(self, C):
         return self.layers(C)
 
+
 class TCN(nn.Module):
-    def __init__(self, node_indim, edge_indim, xc_outdim,
-                 predict_track_params=False):
+    def __init__(self, node_indim, edge_indim, xc_outdim, predict_track_params=False):
         super(TCN, self).__init__()
-        self.in_w1 = IN(node_indim, edge_indim, 
-                        node_outdim=5, edge_outdim=4, 
-                        hidden_size=80)
-        self.in_w2 = IN(5, 4, 
-                        node_outdim=5, edge_outdim=4, 
-                        hidden_size=80)
-        self.in_c1 = IN(5, 13, 
-                        node_outdim=5, edge_outdim=8, 
-                        hidden_size=50)
-        self.in_c2 = IN(5, 8, 
-                        node_outdim=5, edge_outdim=8, 
-                        hidden_size=50)
-        self.in_c3 = IN(5, 8, 
-                        node_outdim=5, edge_outdim=8, 
-                        hidden_size=50)
+        self.in_w1 = IN(
+            node_indim, edge_indim, node_outdim=5, edge_outdim=4, hidden_size=80
+        )
+        self.in_w2 = IN(5, 4, node_outdim=5, edge_outdim=4, hidden_size=80)
+        self.in_c1 = IN(5, 13, node_outdim=5, edge_outdim=8, hidden_size=50)
+        self.in_c2 = IN(5, 8, node_outdim=5, edge_outdim=8, hidden_size=50)
+        self.in_c3 = IN(5, 8, node_outdim=5, edge_outdim=8, hidden_size=50)
         self.W = MLP(12, 1, 80)
         self.B = MLP(20, 1, 80)
         self.X = MLP(20, xc_outdim, 80)
@@ -51,30 +44,24 @@ class TCN(nn.Module):
             self.P = MLP(20, 2, 80)
             self.Q = MLP(20, 1, 20)
         self.predict_track_params = predict_track_params
-        
-    def forward(self, x: Tensor, edge_index: Tensor, 
-                edge_attr: Tensor) -> Tensor:
-        
+
+    def forward(self, x: Tensor, edge_index: Tensor, edge_attr: Tensor) -> Tensor:
+
         # re-embed the graph twice with add aggregation
         x1, edge_attr_1 = self.in_w1(x, edge_index, edge_attr)
         x2, edge_attr_2 = self.in_w2(x1, edge_index, edge_attr_1)
-        
+
         # combine all edge features, use to predict edge weights
-        initial_edge_attr = torch.cat([edge_attr, edge_attr_1, 
-                                       edge_attr_2], dim=1)
+        initial_edge_attr = torch.cat([edge_attr, edge_attr_1, edge_attr_2], dim=1)
         edge_weights = torch.sigmoid(self.W(initial_edge_attr))
 
         # combine edge weights with original edge features
-        edge_attr_w = torch.cat([edge_weights, 
-                                 initial_edge_attr], dim=1)
+        edge_attr_w = torch.cat([edge_weights, initial_edge_attr], dim=1)
 
-        xc1, edge_attr_c1 = self.in_c1(x, edge_index, 
-                                       edge_attr_w)
-        xc2, edge_attr_c2 = self.in_c2(xc1, edge_index, 
-                                       edge_attr_c1)
-        xc3, edge_attr_c3 = self.in_c3(xc2, edge_index, 
-                                       edge_attr_c2)
-        all_xc = torch.cat([x,xc1,xc2,xc3], dim=1)
+        xc1, edge_attr_c1 = self.in_c1(x, edge_index, edge_attr_w)
+        xc2, edge_attr_c2 = self.in_c2(xc1, edge_index, edge_attr_c1)
+        xc3, edge_attr_c3 = self.in_c3(xc2, edge_index, edge_attr_c2)
+        all_xc = torch.cat([x, xc1, xc2, xc3], dim=1)
         beta = torch.sigmoid(self.B(all_xc))
         xc = self.X(all_xc)
         if self.predict_track_params:
@@ -84,4 +71,3 @@ class TCN(nn.Module):
             return edge_weights, xc, beta, p
 
         return edge_weights, xc, beta
-        
--- train_IN.py
+++ train_IN.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import sys
 import argparse
 import logging
@@ -19,45 +19,108 @@ from utils.train_utils import *
 
 initialize_logger(verbose=False)
 
+
 def parse_args(args):
-    parser = argparse.ArgumentParser(description='PyG Interaction Network Implementation')
-    parser.add_argument('-i', '--indir', type=str, default='graphs/train1_ptmin1',
-                        help='input graph directory')
-    parser.add_argument('--n-train', type=int, default=8000)
-    parser.add_argument('--n-test', type=int, default=2000)
-    parser.add_argument('--n-val', type=int, default=250)
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--model-outdir', type=str, default='train_output/models/EC1/',
-                        help='directory in which to save models')
-    parser.add_argument('--stats-outdir', type=str, default='train_output/stats/EC1/',
-                        help='directory in which to save stats')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=75, metavar='N',
-                        help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=0.0005, metavar='LR',
-                        help='learning rate (default: 5*10**-4)')
-    parser.add_argument('--gamma', type=float, default=0.99, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--step-size', type=int, default=5,
-                        help='Learning rate step size')
-    parser.add_argument('--pt-min', type=str, default='1',
-                        help='Cutoff pt value in GeV (default: 1 GeV)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
-    parser.add_argument('--sample', type=int, default=1,
-                        help='TrackML train_{} sample to train on')
-    parser.add_argument('--hidden-size', type=int, default=40,
-                        help='Number of hidden units per layer')
+    parser = argparse.ArgumentParser(
+        description="PyG Interaction Network Implementation"
+    )
+    parser.add_argument(
+        "-i",
+        "--indir",
+        type=str,
+        default="graphs/train1_ptmin1",
+        help="input graph directory",
+    )
+    parser.add_argument("--n-train", type=int, default=8000)
+    parser.add_argument("--n-test", type=int, default=2000)
+    parser.add_argument("--n-val", type=int, default=250)
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--model-outdir",
+        type=str,
+        default="train_output/models/EC1/",
+        help="directory in which to save models",
+    )
+    parser.add_argument(
+        "--stats-outdir",
+        type=str,
+        default="train_output/stats/EC1/",
+        help="directory in which to save stats",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=75,
+        metavar="N",
+        help="number of epochs to train (default: 14)",
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=0.0005,
+        metavar="LR",
+        help="learning rate (default: 5*10**-4)",
+    )
+    parser.add_argument(
+        "--gamma",
+        type=float,
+        default=0.99,
+        metavar="M",
+        help="Learning rate step gamma (default: 0.7)",
+    )
+    parser.add_argument(
+        "--step-size", type=int, default=5, help="Learning rate step size"
+    )
+    parser.add_argument(
+        "--pt-min",
+        type=str,
+        default="1",
+        help="Cutoff pt value in GeV (default: 1 GeV)",
+    )
+    parser.add_argument(
+        "--no-cuda", action="store_true", default=False, help="disables CUDA training"
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        default=False,
+        help="quickly check a single pass",
+    )
+    parser.add_argument(
+        "--seed", type=int, default=1, metavar="S", help="random seed (default: 1)"
+    )
+    parser.add_argument(
+        "--log-interval",
+        type=int,
+        default=10,
+        metavar="N",
+        help="how many batches to wait before logging training status",
+    )
+    parser.add_argument(
+        "--save-model",
+        action="store_true",
+        default=False,
+        help="For Saving the current Model",
+    )
+    parser.add_argument(
+        "--sample", type=int, default=1, help="TrackML train_{} sample to train on"
+    )
+    parser.add_argument(
+        "--hidden-size", type=int, default=40, help="Number of hidden units per layer"
+    )
 
     args = parse_args(args)
 
@@ -67,44 +130,51 @@ def train(args, model, device, train_loader, optimizer, epoch):
     losses, t0, N = [], time(), len(train_loader)
     for batch_idx, (data, fname) in enumerate(train_loader):
         data = data.to(device)
-        if (len(data.x)==0): continue
+        if len(data.x) == 0:
+            continue
         optimizer.zero_grad()
         output = model(data.x, data.edge_index, data.edge_attr)
         y, output = data.y, output.squeeze(1)
-        loss = F.binary_cross_entropy(output, y, reduction='mean')
+        loss = F.binary_cross_entropy(output, y, reduction="mean")
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
-            percent_complete = 100. * batch_idx / N
-            logging.info(f'Train Epoch: {epoch} [{batch_idx}/{N}' +
-                         f'({percent_complete:.0f}%)]\tLoss: {loss.item():.6f}')
-            if args.dry_run: break
+            percent_complete = 100.0 * batch_idx / N
+            logging.info(
+                f"Train Epoch: {epoch} [{batch_idx}/{N}"
+                + f"({percent_complete:.0f}%)]\tLoss: {loss.item():.6f}"
+            )
+            if args.dry_run:
+                break
         losses.append(loss.item())
-    logging.info(f'Epoch completed in {time()-t0}s')
-    logging.info(f'Train Loss: {np.nanmean(losses)}')
+    logging.info(f"Epoch completed in {time()-t0}s")
+    logging.info(f"Train Loss: {np.nanmean(losses)}")
     return np.nanmean(losses)
+
 
 def validate(model, device, val_loader):
     model.eval()
     opt_thlds, accs = [], []
     for batch_idx, (data, fname) in enumerate(val_loader):
         data = data.to(device)
-        if (len(data.x)==0): continue
+        if len(data.x) == 0:
+            continue
         output = model(data.x, data.edge_index, data.edge_attr)
         y, output = data.y, output.squeeze()
-        loss = F.binary_cross_entropy(output, y, reduction='mean').item()
+        loss = F.binary_cross_entropy(output, y, reduction="mean").item()
         diff, opt_thld, opt_acc = 100, 0, 0
         best_tpr, best_tnr = 0, 0
         for thld in np.arange(0.01, 0.6, 0.01):
             acc, TPR, TNR = binary_classification_stats(output, y, thld)
-            delta = abs(TPR-TNR)
-            if (delta.item() < diff): 
+            delta = abs(TPR - TNR)
+            if delta.item() < diff:
                 diff, opt_thld, opt_acc = delta.item(), thld, acc.item()
         opt_thlds.append(opt_thld)
         accs.append(opt_acc)
-    logging.info(f'Validation set accuracy (where TPR=TNR): {np.nanmean(accs)}')
-    logging.info(f'Validation set optimal edge weight thld: {np.nanmean(opt_thld)}')
-    return np.nanmean(opt_thlds) 
+    logging.info(f"Validation set accuracy (where TPR=TNR): {np.nanmean(accs)}")
+    logging.info(f"Validation set optimal edge weight thld: {np.nanmean(opt_thld)}")
+    return np.nanmean(opt_thlds)
+
 
 def test(model, device, test_loader, thld=0.5):
     model.eval()
@@ -112,60 +182,61 @@ def test(model, device, test_loader, thld=0.5):
     with torch.no_grad():
         for batch_idx, (data, fname) in enumerate(test_loader):
             data = data.to(device)
-            if (len(data.x)==0): continue
+            if len(data.x) == 0:
+                continue
             output = model(data.x, data.edge_index, data.edge_attr)
             y, output = data.y, output.squeeze()
             acc, TPR, TNR = binary_classification_stats(output, y, thld)
-            loss = F.binary_cross_entropy(output, data.y, 
-                                          reduction='mean')
+            loss = F.binary_cross_entropy(output, data.y, reduction="mean")
             accs.append(acc.item())
             losses.append(loss.item())
-    logging.info(f'Test loss: {np.nanmean(losses):.4f}')
-    logging.info(f'Test accuracy: {np.nanmean(accs):.4f}')
+    logging.info(f"Test loss: {np.nanmean(losses):.4f}")
+    logging.info(f"Test accuracy: {np.nanmean(accs):.4f}")
     return np.nanmean(losses), np.nanmean(accs)
+
 
 def main(args):
     args = parse_args(args)
-    job_name = f'IN_pt{args.pt_min}'
+    job_name = f"IN_pt{args.pt_min}"
 
     use_cuda = not args.no_cuda and torch.cuda.is_available()
-    logging.info(f'Parameter use_cuda={use_cuda}')
+    logging.info(f"Parameter use_cuda={use_cuda}")
     torch.manual_seed(args.seed)
     device = torch.device("cuda" if use_cuda else "cpu")
-        
-    params = {'batch_size': 1, 'shuffle': True, 'num_workers': 4}
-    loaders = get_dataloaders(args.indir, args.n_train, args.n_test,
-                              n_val=args.n_val, shuffle=False, 
-                              params=params)
-        
+
+    params = {"batch_size": 1, "shuffle": True, "num_workers": 4}
+    loaders = get_dataloaders(
+        args.indir,
+        args.n_train,
+        args.n_test,
+        n_val=args.n_val,
+        shuffle=False,
+        params=params,
+    )
+
     model = EdgeClassifier(3, 4).to(device)
     total_trainable_params = sum(p.numel() for p in model.parameters())
-    logging.info(f'Trainable params in network: {total_trainable_params}')
-    
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
-    scheduler = StepLR(optimizer, step_size=args.step_size,
-                       gamma=args.gamma)
+    logging.info(f"Trainable params in network: {total_trainable_params}")
 
-    output = {'train_loss': [], 'test_loss': [], 'test_acc': []}
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
+
+    output = {"train_loss": [], "test_loss": [], "test_acc": []}
     for epoch in range(1, args.epochs + 1):
-        logging.info(f'Entering epoch {epoch}')
-        train_loss = train(args, model, device, loaders['train'], optimizer, epoch)
-        thld = validate(model, device, loaders['val'])
-        logging.info(f'Sending thld={thld} to test routine.')
-        test_loss, test_acc = test(model, device, loaders['test'], thld=thld)
+        logging.info(f"Entering epoch {epoch}")
+        train_loss = train(args, model, device, loaders["train"], optimizer, epoch)
+        thld = validate(model, device, loaders["val"])
+        logging.info(f"Sending thld={thld} to test routine.")
+        test_loss, test_acc = test(model, device, loaders["test"], thld=thld)
         scheduler.step()
         if args.save_model:
-            model_name = join(args.model_outdir,
-                              job_name + f'_epoch{epoch}')
+            model_name = join(args.model_outdir, job_name + f"_epoch{epoch}")
             torch.save(model.state_dict(), model_name)
-        output['train_loss'].append(train_loss)
-        output['test_loss'].append(test_loss)
-        output['test_acc'].append(test_acc)
+        output["train_loss"].append(train_loss)
+        output["test_loss"].append(test_loss)
+        output["test_acc"].append(test_acc)
         np.save(join(args.stats_outdir, job_name), output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:])
-
-
-
